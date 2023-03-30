2aa2d5171706e66445278bbef22b4388e96aa00b
jy1746 <127377927+jy1746@users.noreply.github.com>
2023-03-30 13:36:22 +0900

    bbsnote first commit

---
+++ bbsnote/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from bbsnote.models import Board, Comment
+
+class BoardForm(forms.ModelForm):
+    class Meta:
+        model = Board
+        fields = ['subject','content']
+        # widgets = {
+        #     'subject' : forms.TextInput(attrs={'class':'form-control'}),
+        #     'content' : forms.Textarea(attrs={'class':'form-control','rows':10}),
+        # }
+        # labels = {
+        #     'subject':'제목',
+        #     'content':'내용'
+        # }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content':'댓글내용'
+        }
---
+++ bbsnote/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'bbsnote'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:board_id>/', views.detail, name='detail'),
+    path('comment/create/<int:board_id>/', views.comment_create, name='comment_create'),
+    path('board/create/', views.board_create, name='board_create'),
+    path('board/modify/<int:board_id>/', views.board_modify, name='board_modify'),
+    path('board/delete/<int:board_id>', views.board_delete, name='board_delete'),
+    path('comment/modify/<int:comment_id>/', views.comment_modify, name='comment_modify'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
+]
